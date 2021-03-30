a01980d4b99b3e4d2bab1bc47088c213ce4be938
Demitri Ovro <Daovro@gmail.com>
2021-03-30 11:11:47 -0700

    added title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'questionnaire', component :QuestionnaireComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
