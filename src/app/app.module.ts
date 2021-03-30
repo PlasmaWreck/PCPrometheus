a04226dca5cf362a5285fe4d11a0d42d3de55cb1
Demitri Ovro <Daovro@gmail.com>
2021-03-30 10:47:16 -0700

    Added questionnaire component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionnaireComponent
   ],
   imports: [
     BrowserModule,
