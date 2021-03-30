62366c6e73286ad19c2f09c5170d74a4443e759f
PlasmaWreck <31967552+PlasmaWreck@users.noreply.github.com>
2021-03-29 17:36:21 -0700

    All things pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PartsdescriptionComponent } from './pages/partsdescription/partsdescription.component';
+import { ResourcetabsComponent } from './components/resourcetabs/resourcetabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartsdescriptionComponent,
+    ResourcetabsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/resourcetabs/resourcetabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resourcetabs',
+  templateUrl: './resourcetabs.component.html',
+  styleUrls: ['./resourcetabs.component.css']
+})
+export class ResourcetabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
