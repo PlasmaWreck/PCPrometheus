ce39549253d568a569197d0c1b61c62e6420167c
Matthew-J-Mendoza <mattjm112999@gmail.com>
2021-03-31 16:35:08 -0700

    Generate item list based on array

--- src/app/pages/computer-building/computer-building.component.ts
+++ src/app/pages/computer-building/computer-building.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DataService } from 'src/app/service/data.service';
 
 @Component({
@@ -15,5 +15,39 @@ export class ComputerBuildingComponent implements OnInit {
     this.dService.GetData().subscribe(data=>console.log(data));
   }
 
-  
+  itemArray = [
+    {
+      "name": "Fart",
+      "Manufacturer": "Ryzen",
+      "Cores":12,
+      "GHz":5,
+      "Threads":55,
+      "Price":349.99
+    },
+    {
+      "name": "Per Mega Shit",
+      "Manufacturer": "I don't know",
+      "Cores":1,
+      "GHz":43,
+      "Threads":14,
+      "Price":123.99
+    },
+    {
+      "name": "Snake Kills dumble dor",
+      "Manufacturer": "yeah",
+      "Cores":8,
+      "GHz":5,
+      "Threads":14,
+      "Price":123123.99
+    },
+    {
+      "name": "ahjksdhf",
+      "Manufacturer": "kicking",
+      "Cores":8,
+      "GHz":31,
+      "Threads":14,
+      "Price":3429.99
+    },
+  ]
+
 }
